15dd90b8bbf6170332a9484e4e57faac5350ec79
Tanvir Rahman <tanvir.rahman.workmail@gmail.com>
2025-10-08 23:37:00 +0600

    Update StoryBook.java for OOP 1/other

--- notes/OOP 1/other/StoryBook.java
+++ notes/OOP 1/other/StoryBook.java
@@ -1,30 +1,97 @@
-public class StoryBook extends Book
-{
-	String category;
-	StoryBook()
-	{
-		System.out.println("Default constructor of StoryBook");
+public class StoryBook{
+	 String isbn;
+	 String bookTitle;
+	 String authorName;
+	 double price;
+	 int availableQuantity;
+	 String category;
+	 static double discountRate=2.50;
+	
+	StoryBook(){
+		System.out.println("Defult Constructor \n");
 	}
-	StoryBook(String isbn, String bookTitle, String authorName, double price, int availableQuantity, String category)
+	StoryBook(String isbn,String bookTitle,String authorName,double price,int availableQuantity, String category)
 	{
-		super(isbn,bookTitle,authorName,price,availableQuantity);
+		this.isbn=isbn;
+		this.bookTitle=bookTitle;
+		this.authorName=authorName;
+		this.price=price;
+		this.availableQuantity=availableQuantity;
 		this.category=category;
-		System.out.println("Valued constructor of StoryBook");
+		//discountRate++;
 	}
-	public void setCategory(String category)
+	
+	static void setDiscountRate(double rate)
 	{
-		this.category=category;
+		discountRate=rate;
+	 System.out.println("Discount Rate:" +rate +"\n");
 	}
-	public String getCategory()
+	static double getDiscountRate()
 	{
+	 return discountRate;
+	}
+	void setIsbn(String isbn)
+	{
+		isbn=isbn;
+	}
+    void setBookTitle(String bookTitle)
+	{
+		bookTitle=bookTitle;
+	}
+    void setAuthorName(String authorName)
+	{
+		authorName=authorName;
+	}
+    void setPrice(double price)
+	{
+		price=price;
+	}
+    void setAvaiableQuantity(int availableQuantity)
+	{
+		availableQuantity=availableQuantity;
+	}
+    void setCategory(String category)
+	{
+		category=category;
+	}
+	
+	
+	String getIsbn()
+	{
+		return isbn;
+	}
+    String getBookTitle(){
+		return bookTitle;
+	}
+    String getAuthorName(){
+		return authorName;
+	}
+    double getPrice(){
+		return price;
+	}
+    int getAvailableQuantity(){
+		return availableQuantity;
+	}
+    String getCategory(){
 		return category;
 	}
-	public void showDetails()
-	{
-		System.out.println(">>>>>Information about the StoryBook<<<<<");
-		super.showDetails();
-		System.out.println("Category: "+this.category);
+	void addQuantity(int amount){
+		System.out.println("Add Quantity: " +amount);
+		availableQuantity=availableQuantity+amount;
+		System.out.println("Total Quantity: " +availableQuantity);
+	}
+    void sellQuantity(int amount){
+	System.out.println("Sold Quantity: " +amount);
+	availableQuantity=availableQuantity-amount;
+    System.out.println("Available Quantity: " +availableQuantity);
+	}
+    void showDetails(){
+		System.out.println("ISBN: " +isbn);
+		System.out.println("Book Title: " +bookTitle);
+		System.out.println("Author Name: " +authorName);
+		System.out.println("Price: " +price+"$");
+		System.out.println("Available Quantity: " +availableQuantity);
+		System.out.println("Category: " +category);
+	
 	}
 }
-		
-		
