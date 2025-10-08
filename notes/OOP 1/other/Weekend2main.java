4eabafd9ebb99670c6b3b4ad5ac89a619733365d
Tanvir Rahman <tanvir.rahman.workmail@gmail.com>
2025-10-08 23:38:08 +0600

    Add Weekend2main.java for OOP 1/other

---
+++ notes/OOP 1/other/Weekend2main.java
@@ -0,0 +1,28 @@
+public class Week2Main
+  
+{
+       Week2Main(String message){
+  
+             System.out.print("A wish for you: "+message);
+  
+       }
+  
+       Week2Main(int week){
+  
+       System.out.print("Welcome to week "+ week + "Lab exercise. ");
+  
+}
+  
+public static void main(String args[]) {
+  
+ 
+  
+        Week2Main obj1 = new Week2Main("Stay safe");     
+  
+        Week2Main obj2 = new Week2Main("Live long" );
+  
+              
+  
+}
+  
+}
