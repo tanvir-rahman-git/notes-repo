68e18d811c09240b790f616caf93ac23aa5dabfc
Tanvir Rahman <tanvir.rahman.workmail@gmail.com>
2025-10-08 23:37:30 +0600

    Update TextBook.java for OOP 1/other

--- notes/OOP 1/other/TextBook.java
+++ notes/OOP 1/other/TextBook.java
@@ -1,99 +1,34 @@
-public class TextBook
-{
-	String isbn;
-	String bookTitle;
-	String authorName;
-	double price;
-	int availableQuantity;
-	int standard;
-	static double discountRate;
+import java.lang.*;
+
+public class TextBook extends Book
+{	
 	
-	TextBook(){
-		System.out.println("Defult Constructor\n");
-	}
+	private int standard;
 	
-	TextBook(String isbn, String bookTitle, String authorName, double price, int availableQuantity, int standard)
+	public TextBook(){ }
+	
+	public TextBook(String isbn, String bookTitle, String authorName, double price, int availableQuantity,int standard)
 	{
-		this.isbn=isbn;
-		this.bookTitle=bookTitle;
-		this.authorName=authorName;
-		this.price=price;
-		this.availableQuantity=availableQuantity;
+	    super(isbn,  bookTitle,  authorName,  price,  availableQuantity);
 		this.standard=standard;
 	}
-	static void setDiscountRate(double rate)
+	
+	public void setStandard(int standard)
 	{
-		discountRate=rate;
-	 System.out.println("Discount Rate:" +rate);
+		this.standard = standard;
 	}
-	static double getDiscountRate()
-	{
-	 return discountRate;
-	}
-	void setIsbn(String isbn)
-	{
-		isbn=isbn;
-	}
-	void setBookTitle(String bookTitle)
-	{
-		bookTitle=bookTitle;
-	}
-	void setAuthorName(String authorName)
-	{
-		authorName=authorName;
-	}
-	void setPrice(double price)
-	{
-		price=price;
-	}
-	void setAvaiableQuantity(int availableQuantity)
-	{
-		availableQuantity=availableQuantity;
-	}
-	void setStandard(int standard)
-	{
-		standard=standard;
-	}
-	String getIsbn()
-	{
-		return isbn;
-	}
-	String getBookTitle()
-	{
-		return bookTitle;
-	}
-	String getAuthorName()
-	{
-		return authorName;
-	}
-	double getPrice()
-	{
-		return price;
-	}
-	int getAvailableQuantity()
-	{
-		return availableQuantity;
-	}
-	int getStandard()
+	
+	
+	public int getStandard()
 	{
 		return standard;
 	}
-	void addQuantity(int amount){
-		System.out.println("Add Quantity: " +amount);
-		availableQuantity=availableQuantity+amount;
-		System.out.println("Total Quantity: " +availableQuantity);
+	
+	public void showDetails()
+	{
+		super.showDetails();
+		System.out.println("Standard  : " +standard );
+		System.out.println();
+		
 	}
-    void sellQuantity(int amount){
-	System.out.println("Sold Quantity: " +amount);
-	availableQuantity=availableQuantity-amount;
-    System.out.println("Sold Quantity: " +availableQuantity);
-	}
-	void showDetails(){
-		System.out.println("ISBN: " +isbn);
-		System.out.println("Book Title: " +bookTitle);
-		System.out.println("Author Name: " +authorName);
-		System.out.println("Price: " +price+"TK.");
-		System.out.println("Available Quantity: " +availableQuantity);
-		System.out.println("Standard: " +standard+"Th");
-	}	
 }
