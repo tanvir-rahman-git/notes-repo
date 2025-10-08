d34511e8ce273e908d0b2152ea4d6670724658c5
Tanvir Rahman <tanvir.rahman.workmail@gmail.com>
2025-10-08 23:37:36 +0600

    Update Book.java for OOP 1/other

--- notes/OOP 1/other/Book.java
+++ notes/OOP 1/other/Book.java
@@ -1,96 +1,94 @@
-import java.lang.*;
+class Book{
+
+private String isbn;
+private String bookTitle;
+private String authorName;
+private double price;
+private int availableQuantity;
 
 
-public  abstract class Book implements BookOperations
-{	
-	private String isbn;
-	private String bookTitle;
-	private String authorName;
-	private double price;
-	private int availableQuantity;
-	public Book(){}
-	public Book(String isbn, String bookTitle, String authorName, double price, int availableQuantity)
-	{
-	
-		this.isbn = isbn;
-		this.bookTitle = bookTitle;
-		this.authorName =authorName;
-		this.price = price;
-		this.availableQuantity =availableQuantity;
-	}
-	
-	public void setIsbn(String isbn)
-	{
-		this.isbn = isbn;
-	}
-	public void setBookTitle(String bookTitle)
-	{
-		this.bookTitle = bookTitle;
-	}
-	public void setAuthorName(String authorName)
-	{
-		this.authorName =authorName;
-	}
-	public void setPrice(double price)
-	{
-		this. price = price;
-	}
-	public void setAvailableQuantity(int availableQuantity)
-	{
-		this.availableQuantity =availableQuantity;
-	}
-	
-	public String getIsbn()
-	{
-		return isbn;
-	}
-	public String getBookTitle ()
-	{
-		return bookTitle;
-	}
-	public String getAuthorName()
-	{
-		return authorName;
-	}
-	public double getPrice()
-	{
-		return price;
-	}
-	public int getAvailableQuantity()
-	{
-		return availableQuantity;
-	}
-	
-	
-	public void addQuantity(int amount)
-    {
-	if(amount>0)
-	{
-		availableQuantity = availableQuantity + amount;
-	}
-	else
-	{
-		System.out.println("Stockout");
-	}
-    }
-    public void sellQuantity(int amount)
-    {
-	if(amount>0 && amount<=availableQuantity)
-	{
-		availableQuantity = availableQuantity - amount;
-	}
-	else
-	{
-		System.out.println("Stockout");
-	}
-    }
+Book( ){
+           System.println.out("empty constructor   :-")
+};
+Book(String isbn1, String bookTitle1, String authorName1, double price1, int availableQuantity1){
 
-	public void showDetails()
-	{
-		System.out.println("International Standard Book Number : " +isbn);
-		System.out.println("Book Title : " +bookTitle );
-		System.out.println("Author Name : " +authorName );
-		System.out.println("Price  : " +price );
-		System.out.println("Available Quantity : " +availableQuantity);
-	}
+this.isbn = isbn1 ;
+this.bookTitle =  bookTitle1 ;
+this.authorName = authorName1 ;
+this.price = price1 ;
+this.availableQuantity = availableQuantity1 ;
+
+}
+void setIsbn(String isbn1){
+
+this.isbn = isbn1 ;
+
+}
+void setBookTitle(String bookTitle1){
+
+this.bookTitle =  bookTitle1 ;
+
+}
+void setAuthorName(String authorName1){
+
+this.authorName = authorName1 ;
+
+}
+void setPrice(double price1){
+
+this.price = price1 ;
+
+}
+void setAvaiableQuantity(int availableQuantity1){
+
+this.availableQuantity = availableQuantity1 ;
+
+}
+String getIsbn( ){
+
+return isbn;
+
+}
+String getBookTitle( ){
+
+return bookTitle;
+
+}
+String getAuthorName( ){
+
+return authorName;
+
+}
+double getPrice( ){
+
+return price;
+
+}
+int getAvailableQuantity( ){
+
+return availableQuantity;
+
+}
+void addQuantity(int amount){
+
+this.availableQuantity = availableQuantity + amount;
+System.out.println( amount + "books has been added. New available book quantity is      : " + availableQuantity);
+
+}
+void sellQuantity(int amount){
+
+this.availableQuantity = availableQuantity - amount;
+System.out.println( amount + "books has been sold. New available book quantity is       : " + availableQuantity);
+
+}
+void showDetails( ){
+
+  System.out.println("isbn              : " + isbn + " .");  
+  System.out.println("bookTitle         : " + bookTitle + " .");  
+  System.out.println("authorName        : " + authorName + " .");  
+  System.out.println("price             : " + price + " .");  
+  System.out.println("availableQuantity : " + availableQuantity + " . \n \n");  
+
+}
+
 }
