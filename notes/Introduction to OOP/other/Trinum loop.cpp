6e12c08da65d7ecfb885dcad53abad04745384bb
Tanvir Rahman <tanvir.rahman.workmail@gmail.com>
2025-10-08 23:28:05 +0600

    Add Trinum loop.cpp for Introduction to OOP/other

---
+++ notes/Introduction to OOP/other/Trinum loop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+int n, triNum;
+triNum = 0;
+
+for ( n = 1; n <= 200; n = n + 1 )
+	 triNum = triNum + n;
+
+cout<<"The 200th triangular number = " << triNum;
+return 0;
+}
