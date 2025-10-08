dae3e51c5fb7f016eac44414bf5d64e6fa81add9
Tanvir Rahman <tanvir.rahman.workmail@gmail.com>
2025-10-08 23:27:16 +0600

    Add forLoop.cpp for Introduction to OOP/other

---
+++ notes/Introduction to OOP/other/forLoop.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+    int i;
+    int j;
+    int sum=0;
+    int Number;
+    cin>>i>>j;
+//N=18
+
+    for(sum=0;sum<=Number;Number++){
+        if(i%2==0)
+        {
+        cout<<i<<endl;
+        }
+            if(j%2==0){
+                cout<<j<<endl;
+        }
+
+        sum =i+j;//sum=18
+        cout<<sum<<endl;
+    }
+return 0;
+}
