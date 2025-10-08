6311becdffd54b9f265285058cab3ec29669a411
Tanvir Rahman <tanvir.rahman.workmail@gmail.com>
2025-10-08 23:28:02 +0600

    Add thftfyhj.cpp for Introduction to OOP/other

---
+++ notes/Introduction to OOP/other/thftfyhj.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+      for (int i=1;i<=5;i++){
+
+           if(i==5)
+            continue;
+           cout<<"ans = "<<i ;
+      }
+
+       return 0;
+}
