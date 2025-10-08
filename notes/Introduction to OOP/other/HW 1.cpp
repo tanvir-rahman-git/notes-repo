0b0bd695a55b2230434aad84b0c22031b256fad4
Tanvir Rahman <tanvir.rahman.workmail@gmail.com>
2025-10-08 23:27:20 +0600

    Add HW 1.cpp for Introduction to OOP/other

---
+++ notes/Introduction to OOP/other/HW 1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+       int N;
+
+       cout<<"Enter any Real Number:"<<endl;
+
+       cin>>N;
+
+       for(int i=1;i<=10;i++){
+
+           cout<<N<<"*"<<i<<"="<<N*i<<endl;
+
+           }
+
+        return 0;
+}
