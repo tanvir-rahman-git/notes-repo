7e66bf38008b5c4b55ac5674554e055b296aef05
Tanvir Rahman <tanvir.rahman.workmail@gmail.com>
2025-10-08 23:38:06 +0600

    Add quiz.java for OOP 1/other

---
+++ notes/OOP 1/other/quiz.java
@@ -0,0 +1,8 @@
+public class quiz
+{
+                public static void main(String arg[])
+                {
+                                System.out.print("The Number is: "+ 8+2) ;
+                                System.out.print(4+5) ;
+                }
+}
