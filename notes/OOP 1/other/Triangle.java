8940586ea4e5a74ea37dc48015d88a666f4abd97
Tanvir Rahman <tanvir.rahman.workmail@gmail.com>
2025-10-08 23:38:31 +0600

    Add Triangle.java for OOP 1/other

---
+++ notes/OOP 1/other/Triangle.java
@@ -0,0 +1,38 @@
+public class Triangle 
+{
+	private int x;
+	private int y;
+	private int z;
+	
+	public Triangle()//Empty Constructor
+	{
+		System.out.println("Empty Constructor" +"\n");
+	}
+	
+	public Triangle(int x,int y,int z)
+	{ //Valued constructor
+		System.out.println("Valued Constructor" +"\n");
+		this.x=x;
+		this.y=y;
+		this.z=z;
+	}
+	
+	public void testTriangle()//if else statement for findout equilateral,isosceles,scalene
+	{
+		if(x==y&&y==z){ 
+			System.out.println("The Triangle is equilateral" +"\n");
+		}
+		else if(x==y||x==z||y==z){
+			System.out.println("The Triangle is isosceles" +"\n");
+		}
+		else{
+			System.out.println("The Triangle is scalene " +"\n");
+		}
+	}
+	public void showInfo()
+	{
+		System.out.println("info>>>>>>>>>");
+		System.out.println("X: "+this.x+"\n"+"Y: "+this.y+"\n"+"Z: "+this.z);
+	}
+
+}
