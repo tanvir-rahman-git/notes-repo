68be03673a96f905716114539a450e7004468ca5
Tanvir Rahman <tanvir.rahman.workmail@gmail.com>
2025-10-08 23:37:01 +0600

    Update TextBook.java for OOP 1/other

--- notes/OOP 1/other/TextBook.java
+++ notes/OOP 1/other/TextBook.java
@@ -1,30 +1,99 @@
-public class TextBook extends Book
+public class TextBook
 {
+	String isbn;
+	String bookTitle;
+	String authorName;
+	double price;
+	int availableQuantity;
 	int standard;
-	TextBook()
-	{
-		System.out.println("Default constructor of TextBook");
+	static double discountRate;
+	
+	TextBook(){
+		System.out.println("Defult Constructor\n");
 	}
+	
 	TextBook(String isbn, String bookTitle, String authorName, double price, int availableQuantity, int standard)
 	{
-		super(isbn,bookTitle,authorName,price,availableQuantity);
+		this.isbn=isbn;
+		this.bookTitle=bookTitle;
+		this.authorName=authorName;
+		this.price=price;
+		this.availableQuantity=availableQuantity;
 		this.standard=standard;
-		System.out.println("Valued constructor of TextBook");
 	}
-	public void setStandard(int standard)
+	static void setDiscountRate(double rate)
 	{
-		this.standard=standard;
+		discountRate=rate;
+	 System.out.println("Discount Rate:" +rate);
 	}
-	public int getStandard()
+	static double getDiscountRate()
+	{
+	 return discountRate;
+	}
+	void setIsbn(String isbn)
+	{
+		isbn=isbn;
+	}
+	void setBookTitle(String bookTitle)
+	{
+		bookTitle=bookTitle;
+	}
+	void setAuthorName(String authorName)
+	{
+		authorName=authorName;
+	}
+	void setPrice(double price)
+	{
+		price=price;
+	}
+	void setAvaiableQuantity(int availableQuantity)
+	{
+		availableQuantity=availableQuantity;
+	}
+	void setStandard(int standard)
+	{
+		standard=standard;
+	}
+	String getIsbn()
+	{
+		return isbn;
+	}
+	String getBookTitle()
+	{
+		return bookTitle;
+	}
+	String getAuthorName()
+	{
+		return authorName;
+	}
+	double getPrice()
+	{
+		return price;
+	}
+	int getAvailableQuantity()
+	{
+		return availableQuantity;
+	}
+	int getStandard()
 	{
 		return standard;
 	}
-	public void showDetails()
-	{
-		System.out.println(">>>>>Information about the TextBook<<<<<");
-		super.showDetails();
-		System.out.println("Standard: "+this.standard);
+	void addQuantity(int amount){
+		System.out.println("Add Quantity: " +amount);
+		availableQuantity=availableQuantity+amount;
+		System.out.println("Total Quantity: " +availableQuantity);
 	}
+    void sellQuantity(int amount){
+	System.out.println("Sold Quantity: " +amount);
+	availableQuantity=availableQuantity-amount;
+    System.out.println("Sold Quantity: " +availableQuantity);
+	}
+	void showDetails(){
+		System.out.println("ISBN: " +isbn);
+		System.out.println("Book Title: " +bookTitle);
+		System.out.println("Author Name: " +authorName);
+		System.out.println("Price: " +price+"TK.");
+		System.out.println("Available Quantity: " +availableQuantity);
+		System.out.println("Standard: " +standard+"Th");
+	}	
 }
-		
-		
