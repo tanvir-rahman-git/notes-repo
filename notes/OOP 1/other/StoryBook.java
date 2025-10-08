35945f660a7db3efccf213063d244515caf2694c
Tanvir Rahman <tanvir.rahman.workmail@gmail.com>
2025-10-08 23:37:26 +0600

    Update StoryBook.java for OOP 1/other

--- notes/OOP 1/other/StoryBook.java
+++ notes/OOP 1/other/StoryBook.java
@@ -1,97 +1,35 @@
-public class StoryBook{
-	 String isbn;
-	 String bookTitle;
-	 String authorName;
-	 double price;
-	 int availableQuantity;
-	 String category;
-	 static double discountRate=2.50;
+import java.lang.*;
+
+public class StoryBook extends Book
+{	
 	
-	StoryBook(){
-		System.out.println("Defult Constructor \n");
-	}
-	StoryBook(String isbn,String bookTitle,String authorName,double price,int availableQuantity, String category)
+	private String category;
+	
+	public StoryBook(){ }
+	
+	public StoryBook(String isbn, String bookTitle, String authorName, double price, int availableQuantity,String category)
 	{
-		this.isbn=isbn;
-		this.bookTitle=bookTitle;
-		this.authorName=authorName;
-		this.price=price;
-		this.availableQuantity=availableQuantity;
+	
+		super(isbn,  bookTitle,  authorName,  price,  availableQuantity);
+		this.category = category;
+	}
+	
+	
+	public void setCategory(String category)
+	{
 		this.category=category;
-		//discountRate++;
-	}
-	
-	static void setDiscountRate(double rate)
-	{
-		discountRate=rate;
-	 System.out.println("Discount Rate:" +rate +"\n");
-	}
-	static double getDiscountRate()
-	{
-	 return discountRate;
-	}
-	void setIsbn(String isbn)
-	{
-		isbn=isbn;
-	}
-    void setBookTitle(String bookTitle)
-	{
-		bookTitle=bookTitle;
-	}
-    void setAuthorName(String authorName)
-	{
-		authorName=authorName;
-	}
-    void setPrice(double price)
-	{
-		price=price;
-	}
-    void setAvaiableQuantity(int availableQuantity)
-	{
-		availableQuantity=availableQuantity;
-	}
-    void setCategory(String category)
-	{
-		category=category;
 	}
 	
 	
-	String getIsbn()
+	public String getCategory()
 	{
-		return isbn;
-	}
-    String getBookTitle(){
-		return bookTitle;
-	}
-    String getAuthorName(){
-		return authorName;
-	}
-    double getPrice(){
-		return price;
-	}
-    int getAvailableQuantity(){
-		return availableQuantity;
-	}
-    String getCategory(){
 		return category;
 	}
-	void addQuantity(int amount){
-		System.out.println("Add Quantity: " +amount);
-		availableQuantity=availableQuantity+amount;
-		System.out.println("Total Quantity: " +availableQuantity);
-	}
-    void sellQuantity(int amount){
-	System.out.println("Sold Quantity: " +amount);
-	availableQuantity=availableQuantity-amount;
-    System.out.println("Available Quantity: " +availableQuantity);
-	}
-    void showDetails(){
-		System.out.println("ISBN: " +isbn);
-		System.out.println("Book Title: " +bookTitle);
-		System.out.println("Author Name: " +authorName);
-		System.out.println("Price: " +price+"$");
-		System.out.println("Available Quantity: " +availableQuantity);
-		System.out.println("Category: " +category);
-	
+
+	public void showDetails()
+	{
+		super.showDetails ();
+		System.out.println("Category : " +category);
+		System.out.println();
 	}
 }
