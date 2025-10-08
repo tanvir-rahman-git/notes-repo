4b7654014492c787036a494ed4e7714bbe4fe776
Tanvir Rahman <tanvir.rahman.workmail@gmail.com>
2025-10-08 23:35:40 +0600

    Add A.java for OOP 1/final

---
+++ notes/OOP 1/final/A.java
@@ -0,0 +1,14 @@
+public final class A
+{
+	//public final int x=20;
+	public A()
+	{
+		//this.x=10;
+		//System.out.println("Value of x: "+this.x);
+	}
+	public final void show()
+	{
+		//x=20;
+		//System.out.println("Value of x: "+x);
+	}
+}
