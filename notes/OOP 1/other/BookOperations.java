a822ca402d578d0e80f0ddd50275f592aca44a63
Tanvir Rahman <tanvir.rahman.workmail@gmail.com>
2025-10-08 23:37:11 +0600

    Add BookOperations.java for OOP 1/other

---
+++ notes/OOP 1/other/BookOperations.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+
+
+    public interface BookOperations
+    {
+	    public void addQuantity(int amount);
+	    public void sellQuantity(int amount);
+	    public void showDetails();
+    }
