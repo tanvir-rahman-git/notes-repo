d7fc3648f395ad73ddf2614a365e6d383e3331e1
Tanvir Rahman <tanvir.rahman.workmail@gmail.com>
2025-10-08 23:36:31 +0600

    Add entree.java for OOP 1/other

---
+++ notes/OOP 1/other/entree.java
@@ -0,0 +1,17 @@
+package DataClasses;
+
+
+
+public abstract class entree 
+{
+    public String EntryID;
+    public String EntryName;
+    public String EntryGender;
+    public abstract void get_info();
+        
+    
+    public abstract void out_info();
+        
+    
+}   
+
