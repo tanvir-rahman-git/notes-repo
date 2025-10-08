36d0e68d619bf9914570abffff705394aeaa94d6
Tanvir Rahman <tanvir.rahman.workmail@gmail.com>
2025-10-08 23:36:58 +0600

    Update Start.java for OOP 1/other

--- notes/OOP 1/other/Start.java
+++ notes/OOP 1/other/Start.java
@@ -1,37 +1,30 @@
-public class Start
-{
-	public static void main (String[]args)
-	{
-		Bookshop b1=new Bookshop ("BookStore");
-		TextBook t1= new TextBook("10th Release","James Harden","Tony Zaret",300,20,8);
-		if(b1.insertTextBook(t1))
-		{
-			System.out.println("TextBOOK 1 inserted");
-		}
-		else
-		{
-			System.out.println("TextBOOKs overflow");
-		}
+public class Start{
+	public static void main(String[] args){
 		
-		TextBook t2= new TextBook("354554-a","James Harden","Tony Zaret",300,20,8);
-		b1.insertTextBook(t2);
-		StoryBook s1= new StoryBook("1st Edition","Huckleberry Finn","Charles Dickens",200,50,"Adventure");
-		b1.insertStoryBook(s1);
-		StoryBook s2= new StoryBook("10th Edition","Huckleberry Finn","Charles Dickens",200,50,"Adventure");
-		b1.insertStoryBook(s2);
-		StoryBook s3= new StoryBook("9th Edition","Huckleberry Finn","Charles Dickens",200,50,"Adventure");
-		//b1.insertStoryBook(s3);
-		//b1.showAllTextBooks();
-		//b1.showAllStoryBooks();
-		if(b1.removeStoryBook(s3))
-		{
-			System.out.println("Removed");
-		}
-		else
-		{
-			System.out.println("No match found");
-		}
-		b1.searchTextBook("354554-a").showDetails();
-		//b1.showAllStoryBooks();
+		//StoryBook
+		
+		StoryBook s1= new StoryBook();
+
+		StoryBook s2= new StoryBook("998-0-4848-1325-7","The Guest List","Lucy Foley",50.22,13,"Mystery");
+		s2.showDetails();
+		s2.addQuantity(4);
+		s2.sellQuantity(10);
+		s2.setDiscountRate(9.99);
+		
+		//isbn,String bookTitle,String authorName,double price,int availableQuantity, String category
+		
+		//TextBook
+		
+		TextBook a1= new TextBook();
+		
+		TextBook b1= new TextBook("668-5-985-7", "HSC Math 1st Part","S.U Ahmed",350.05,11,8);
+		b1.showDetails();
+		b1.addQuantity(4);
+		b1.sellQuantity(11);
+		b1.setDiscountRate(21.00);
+		
+		
+		
+		//isbn, String bookTitle, String authorName, double price, int availableQuantity, int standard
 	}
 }
