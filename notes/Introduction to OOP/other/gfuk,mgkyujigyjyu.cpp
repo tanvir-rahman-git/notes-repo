fb2a6cfb4ac23ed4b3d2efcf6cd798af884a49ee
Tanvir Rahman <tanvir.rahman.workmail@gmail.com>
2025-10-08 23:27:18 +0600

    Add gfuk,mgkyujigyjyu.cpp for Introduction to OOP/other

---
+++ notes/Introduction to OOP/other/gfuk,mgkyujigyjyu.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,N;
+    cout<<"Enter the number= "<<endl;
+    cin>>N;
+    while (i=0;i>=N;N++){
+
+        if(i%2==0){
+            cout<<"even number= ";
+        }
+        cout<<i;
+    }
+
+    return 0;
+}
